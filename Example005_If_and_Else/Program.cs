168a8b8b302507925a1e304db21f5bd05ae09683
MusatovDA <damusatov@mail.ru>
2023-04-20 16:41:48 +0300

    Example005_If_and_Else prodjekt

---
+++ Example005_If_and_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя:");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "demon")
+{
+    Console.WriteLine("УРА! Привет бро!");
+}
+else
+{
+    Console.Write("Здравствуйте!, ");
+    Console.WriteLine(username);
+}
