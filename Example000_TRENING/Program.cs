8e1e06223970731cdc12c9b4346f22b2126da22a
MusatovDA <damusatov@mail.ru>
2023-04-20 22:46:23 +0300

    Example000_ TRENING

---
+++ Example000_TRENING/Program.cs
@@ -0,0 +1,62 @@
+﻿// введите число до тысячной
+float f = 5.45445454f;
+Console.WriteLine(Math.Round(f, 3));
+
+
+
+
+// введите число Е с точностью до десятых
+double ine = Math.E;
+Console.WriteLine(Math.Round(ine, 1));
+
+
+
+// ввод числа с клавы с надписью
+Console.WriteLine("Введите число: ");
+string userunput = Console.ReadLine();
+
+
+
+
+// ввод числа с клавы с надписью 2
+Console.WriteLine("Введите число: ");
+string userunput2 = Console.ReadLine();
+Console.WriteLine("вы ввели число, мои поздравления!");
+
+
+
+
+// ввод 3х чисел на строчке с 1 пробелом
+Console.WriteLine("1 25 39");
+
+
+
+
+// ввод 3х чисел на строчке с 2 пробеломи
+Console.WriteLine("2  14  412");
+
+
+
+// вывести на экран 3 числа друг под другом
+Console.WriteLine("122 523 752");
+Console.WriteLine("122 325 451");
+
+
+
+
+// вывести на экран 4 числа друг под другом (столбиком)
+Console.WriteLine("122\n325\n555\n451");
+
+
+
+
+Console.WriteLine("5 10\n7 cm");
+
+
+
+// покраска и вывод разделителя
+ConsoleColor defaultColor = Console.ForegroundColor;
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("________________________________________________________________");
+
+
